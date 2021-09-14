6311287a2df213bd00d8dbe31ea810da78feb6e7
Kelsey Erickson <kerickson@gmx.com>
2021-09-14 22:42:15 +0000

    Add renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
 
   const createTweetElement = (tweetData) => {
-    const $tweet = $(
+    let $tweet = $(
       `<article class="tweet">
         <header>
           <div class="poster-profile">
@@ -44,8 +44,55 @@ $(document).ready(function () {
 
   };
 
-  const $tweet = createTweetElement(tweetData);
-  $('#tweets-container').append($tweet);
+  // Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }, 
+  
+  {
+    "user": {
+      "name": "Kelsey",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@WHAT" },
+    "content": {
+      "text": "I do not speak French"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+  
+
+const renderTweets = (tweets) => {
+
+  tweets.forEach(tweet => {
+    $('#tweets-container').append(createTweetElement(tweet));
+  });
+  
+};
+
+renderTweets(data)
+
 });
 
 
