1c20196e47b940da5acf20c93a9c6b259f384168
Kelsey Erickson <kerickson@gmx.com>
2021-09-15 22:23:41 +0000

    Remove test code and update get request syntax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,45 +5,6 @@
  */
 $(document).ready(function() {
 
-   // Fake data taken from initial-tweets.json
-   const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-
-    {
-      "user": {
-        "name": "Kelsey",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@WHAT"
-      },
-      "content": {
-        "text": "I do not speak French"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const createTweetElement = (tweetData) => {
     const $tweet = $(
       `<article class="tweet">
@@ -77,7 +38,6 @@ $(document).ready(function() {
 
   };
 
-  renderTweets(data)
 
   $("form").on('submit', function(event) {
     event.preventDefault();
@@ -94,12 +54,12 @@ $(document).ready(function() {
 
   const loadTweets = () => {
 
-    $.ajax('/tweets', { method: 'GET' })
+    $.get('/tweets')
     .then(function (tweets) {
-      console.log('Success: ', tweets);
+      renderTweets(tweets);
       
     }).catch((error) => {
-      console.log(error)
+      return error;
     });
 
   }
