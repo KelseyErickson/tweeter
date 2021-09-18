f6c076f01c0c6be01efd6fab442ec0c5c90356bd
Kelsey Erickson <kerickson@gmx.com>
2021-09-18 00:28:16 +0000

    Add comments and fix lint errors

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
+  // To retrieve the character limit from html
+  const $charLimit = $(".counter")[0]["innerHTML"];
 
-  const charLimit = $('.counter')[0]["innerHTML"]
-
-  $('#tweet-text').on("input", function() {
-
-    const $counter = $(this).siblings("div").children(".counter");
-    const $textInput = $(this).val().length;
-    const totalChars = charLimit - $textInput;
+  // On input this will take the character counter and subtract the characters being typed
+  $("#tweet-text").on("input", function() {
+    const $counter = $(this).siblings("div").children(".counter"); // Accessing the counter
+    const $textInput = $(this).val().length; // Accessing the text input from the text-area
+    const totalChars = $charLimit - $textInput;
 
     $($counter).text(totalChars);
 
+    // To add class making text red if the counter is over the limit
     totalChars < 0 ? $counter.addClass("redText") : $counter.removeClass("redText");
 
   });
-
+  
 });
